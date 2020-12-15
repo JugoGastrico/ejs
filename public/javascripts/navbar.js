3a2fdfded43847c1ed6d632a5608527c1be70f1d
Alexander <alexandertisko@hotmail.com>
2020-12-14 23:12:54 -0300

    starting set ups

---
+++ public/javascripts/navbar.js
@@ -0,0 +1,22 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".menu-links");
+  const navLinks = document.querySelectorAll(".menu-links li");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("menu-active");
+    //animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.4
+        }s`;
+        console.log(index / 7);
+      }
+    });
+    //Burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+navSlide();
