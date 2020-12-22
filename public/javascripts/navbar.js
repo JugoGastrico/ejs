3e48773d57c5f0826e8bc1ae4bd0b5325965ea71
Alexander <alexandertisko@hotmail.com>
2020-12-22 01:14:44 -0300

    beforeContactForm

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -9,7 +9,7 @@ const navSlide = () => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
+        link.style.animation = `navLinkFade 0.3s ease forwards ${
           index / 7 + 0.4
         }s`;
         console.log(index / 7);
